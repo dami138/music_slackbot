9b7b2aad8e3180c82468b1b9e3c10c26d6064b75
damu3 <sodam138@naver.com>
2023-12-11 16:25:43 +0900

    음악 좋아요 버튼 구현

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ SUPABASE_URL            = os.getenv('SUPABASE_URL')
 SUPABASE_KEY            = os.getenv('SUPABASE_KEY')     
 GOOGLE_KEY              = os.getenv('GOOGLE_KEY')
 
-client  = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase_client  = create_client(SUPABASE_URL, SUPABASE_KEY)
 youtube = build('youtube', 'v3', developerKey=GOOGLE_KEY)
 
 app = App(
@@ -36,13 +36,93 @@ def message_handler(message, say):
 
 # 아무거나 냅다 하나 추천
 @app.command("/recommend")
-def recommend(ack, say):
+def recommend(ack, body, client):
     ack()
     
-    musics = client.table("music").select("*").execute()
+    musics = supabase_client.table("music").select("*").execute()
     music  = random.choice(musics.data)
 
-    say(f"<@{music['slack_id']}>님의 추천 음악: {music['title']} \n{music['youtube_url']}")
+
+    client.chat_postMessage(
+        channel=body["channel_id"],
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"<@{music['slack_id']}>님의 추천 음악: {music['title']} \n{music['youtube_url']}"
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": ":thumbsup: 좋아요",
+                        },
+                        "action_id": "button_click"  # 버튼 액션 ID
+                    }
+                ]
+            }
+        ]
+    )
+
+@app.action("button_click")
+def handle_thumb_click(ack, body, client):
+    # 요청 확인
+    ack()
+    
+    # 버튼 클릭에 대한 정보
+    user_id = body["user"]["id"]
+    channel_id = body["channel"]["id"]
+    message_ts = body["container"]["message_ts"]
+
+    text = body["message"]["blocks"][0]["text"]["text"]
+    match= re.search('v=([0-9A-Za-z_-]{11})', text)
+    video_id = match.group(1)
+
+    btnName = body["message"]["blocks"][1]["elements"][0]["text"]["text"]
+
+    #supabase에서 해당 음악의 좋아요 수 +1/-1
+    current_value_result = supabase_client.table("music").select("thumb").eq("youtube_url", f"https://www.youtube.com/watch?v={video_id}").execute()
+
+    if btnName == ":thumbsup: 좋아요":
+        new_thumb_value = current_value_result.data[0]['thumb'] + 1
+    else:
+        new_thumb_value = current_value_result.data[0]['thumb'] -1
+
+    supabase_client.table("music").update({"thumb": new_thumb_value}).eq("youtube_url", f"https://www.youtube.com/watch?v={video_id}").execute()
+
+    # 기존 메시지 업데이트
+    new_button_text = ":thumbsup: 좋아요" if btnName == ":thumbsup: 좋아요 취소" else ":thumbsup: 좋아요 취소"
+    client.chat_update(
+        channel=channel_id,
+        ts=message_ts,
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": text
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": new_button_text,
+                        },
+                        "action_id": "button_click"  # 버튼 액션 ID
+                    }
+                ]
+            }
+        ]
+    )
 
 # 음악 추가
 @app.command("/add_music")
@@ -80,7 +160,7 @@ def add_music(ack, say, command):
                     "artist"        :artist,
                     "youtube_url"   :youtube_url}
     
-    client.table("music").insert(music_data).execute()    
+    supabase_client.table("music").insert(music_data).execute()    
     say(f"{music_data['title']} 이(가) 추가되었습니다.")
 
 # 최신 추가된 음악 리스트 n개 반환
@@ -89,7 +169,7 @@ def list_music(ack, say, command):
     ack()
     count = int(command['text'])
 
-    musics = client.table("music").select("*").order("created_at",desc=True).limit(count).execute()
+    musics = supabase_client.table("music").select("*").order("created_at",desc=True).limit(count).execute()
     msg = ""
     for i,music in enumerate(musics.data):
         msg += f"{i+1}: {music['title']} \n{music['youtube_url']} \n"
@@ -103,14 +183,14 @@ def user_music(ack, say, command):
     print(command)
     slack_id = re.search('<@(.*)\|', command['text']).group(1)    
 
-    musics = client.table("music").select("*").eq("slack_id",slack_id).order("created_at",desc=True).limit(3).execute()
+    musics = supabase_client.table("music").select("*").eq("slack_id",slack_id).order("created_at",desc=True).limit(3).execute()
     msg = ""
     for i,music in enumerate(musics.data):
         msg += f"{i+1}: {music['title']} \n{music['youtube_url']} \n"
     say(msg)
     
 
-# todo: 음악 삭제, 추천버튼
+# todo: 음악 삭제
     
 
 
